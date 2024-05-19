d4b67f7ba5c5b0a55419274b694e52b572f013a5
Subhamk2004 <subhamk1978@gmail.com>
2024-05-20 01:05:05 +0530

    changes

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-let arr = ['subham', 12, 34, false, "subham"];
-console.log(arr);
+let buttons = document.querySelectorAll('.button');
+let body = document.querySelector('body');
+
+
+
+
+//
+buttons.forEach((button) => {
+    // console.log(button);
+    button.addEventListener('click', function (event) {
+        console.log(event);
+        console.log(event.target);
+        if(event.target.id === "aqua")
+            {
+                document.querySelector('body').style.backgroundColor = "aquamarine";
+            }
+        else if(event.target.id === "white")
+            {
+                document.querySelector('body').style.backgroundColor = "white";
+            }
+        else if(event.target.id === "gray")
+            {
+                document.querySelector('body').style.backgroundColor = "gray";
+            }
+        else if(event.target.id === "red")
+            {
+                document.querySelector('body').style.backgroundColor = "red";
+            }
+    });
+});
+
+//in the above forEach the loop just attackes the event listener
+// to the buttons , so it executed only 4 times but now the event listener is attached to
+// the buttons so they will work infinitely;
